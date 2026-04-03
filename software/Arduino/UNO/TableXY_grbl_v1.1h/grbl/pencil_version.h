bb5785ffb6ef98c6fc208ce1ad45fb77adab8cb1
Jose Luis <126956029+jlgc-retired@users.noreply.github.com>
2026-04-03 19:26:33 +0200

    Adjusting GRBL version 1.1h

---
+++ software/Arduino/UNO/TableXY_grbl_v1.1h/grbl/pencil_version.h
@@ -0,0 +1,31 @@
+/*
+  gcode.h - rs274/ngc parser.
+  Part of Grbl
+
+  Copyright (c) 2011-2015 Sungeun K. Jeon
+  Copyright (c) 2009-2011 Simen Svale Skogsrud
+  
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef pencil_version_h
+#define pencil_version_h
+
+#define GRBL_PENCIL_VERSION "0.100"
+
+#define GRBL_BUILD_PENCIL_DAY   "03"    //compilation Day
+#define GRBL_BUILD_PENCIL_MONTH "04"    //compilation Month
+#define GRBL_BUILD_PENCIL_YEAR "2026"   //compilation Year
+
+#endif
