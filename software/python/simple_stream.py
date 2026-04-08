a07556d6ab52d5c6f71737d2fb0c004c2335d946
Jose Luis <126956029+jlgc-retired@users.noreply.github.com>
2026-04-08 21:56:58 +0200

    Testing python scripts

--- software/python/simple_stream.py
+++ software/python/simple_stream.py
@@ -40,24 +40,26 @@ import serial
 import time
 
 # Open grbl serial port
-s = serial.Serial('/dev/tty.usbmodem1811',115200)
+s = serial.Serial('/dev/ttyACM0',115200)
 
 # Open g-code file
-f = open('grbl.gcode','r');
+f = open('MultiShape_0029.ngc','r');
 
 # Wake up grbl
-s.write("\r\n\r\n")
+s.write(str.encode('\r\n\r\n'))
 time.sleep(2)   # Wait for grbl to initialize 
 s.flushInput()  # Flush startup text in serial input
 
 # Stream g-code to grbl
 for line in f:
     l = line.strip() # Strip all EOL characters for consistency
-    print 'Sending: ' + l,
-    s.write(l + '\n') # Send g-code block to grbl
+    print ('Sending: ' + l),
+    s.write(str.encode(l)) # Send g-code block to grbl
+    s.write(str.encode('\n'))    
     grbl_out = s.readline() # Wait for grbl response with carriage return
-    print ' : ' + grbl_out.strip()
-
+    print (' : ')
+    print (grbl_out.strip())
+    
 # Wait here until grbl is finished to close serial port and file.
 raw_input("  Press <Enter> to exit and disable grbl.") 
 
